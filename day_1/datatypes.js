38393b2b8abb08aab0185997d363f1361eac6fb3
huynhthanhphat <thanhphat02122003@gmail.com>
2024-03-18 16:54:45 +0700

    day_1

---
+++ day_1/datatypes.js
@@ -0,0 +1,7 @@
+console.log(typeof fname);
+console.log(typeof inRelationship);
+console.log(typeof someVar);
+console.log(typeof number);
+
+console.log(myAge);
+console.log(yourAge);
---
+++ day_1/variable.js
@@ -0,0 +1,38 @@
+// Comments can make code readable
+
+// Welcome to 30DaysOfJavaScript
+
+/*
+Comments can make code readable
+Easy to reuse and informative 
+*/
+
+let fname = "PhatHuynh";
+let inRelationship = true;
+let someVar;
+let number = null;
+
+let var1;
+let var2;
+let var3;
+let var4;
+
+let varA = "abcdef111";
+let varB = 123132;
+let varC = false;
+let varD = 12.21;
+
+let firstName = "Phat";
+let lastName = "Huynh";
+let married = false;
+let country = "VietNam";
+let age = 21;
+
+let FirstName = "Phat",
+    LastName = "Huynh",
+    Married = false,
+    Country = "VietNam",
+    Age = 21;
+
+let myAge = "I am 21 years old.";
+let yourAge = "Your are 27 years old.";
